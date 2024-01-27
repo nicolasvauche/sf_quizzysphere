fdfd1a0ebcb41c77ad69fc2500aa2d8830de31a4
Nicolas vauché <hello@nicolasvauche.net>
2024-01-27 12:25:01 +0100

    Settings as a Twig Service

---
+++ src/Service/SettingsService.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Service;
+
+use App\Entity\Settings;
+use Doctrine\ORM\EntityManagerInterface;
+
+class SettingsService
+{
+    private EntityManagerInterface $entityManager;
+
+    public function __construct(EntityManagerInterface $entityManager)
+    {
+        $this->entityManager = $entityManager;
+    }
+
+    private function getSettings()
+    {
+        return $this->entityManager->getRepository(Settings::class)->find(1);
+    }
+
+    public function getDefaultUserGroup()
+    {
+        return $this->getSettings()->getDefaultUserGroup();
+    }
+}
